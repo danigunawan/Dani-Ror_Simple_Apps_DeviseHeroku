89bfcb3392b123eee5ba4a2b5d3cb4dd3a8fe58e
Dani Gunawan <danigunawan.elektroug@gmail.com>
2017-11-24 23:24:58 +0700

    push to heroku

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -5,12 +5,12 @@ class CommentsController < ApplicationController
   # POST /comments.json
   def create
     @submission = Submission.find(params[:submission_id])
-    @comment = @submission.comment.new(comment_params)
+    @comment = @submission.comments.new(comment_params)
     @comment.user = current_user
 
     respond_to do |format|
       if @comment.save
-        format.html { redirect_to @comment, notice: 'Comment was successfully created.' }
+        format.html { redirect_to @submission, notice: 'Comment was successfully created.' }
         
       else
         format.html { render :new }
--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,13 @@
 Rails.application.routes.draw do
-  
+  resources :comments
   devise_for :users
-  resources :submissions do
-  	member do
-  		put "Like", to: "submission#upvote"
-  		put "dislike", to: "submission#downvote"
-  	end
-  	resources :comments
-  end
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'submissions#index'
+
+  resources :submissions do
+    member do
+      put "like", to: "submissions#upvote"
+      put "dislike", to: "submissions#downvote"
+    end
+    resources :comments
+  end
 end
